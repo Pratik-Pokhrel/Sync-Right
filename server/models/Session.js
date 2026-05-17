80471b66f7555c3e54c0767361f107fb20b9adf6
Pratik-Pokhrel <prateek.pokhrel2@gmail.com>
2026-05-17 08:52:50 +0545

    feat: Schemas completed for now

---
+++ server/models/Session.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const SessionSchema = new Schema(
+  {
+    room: {
+      type: Schema.Types.ObjectId,
+      ref: "Room",
+      required: true,
+    },
+
+    host: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    participants: [
+      {
+        user: { type: Schema.Types.ObjectId, ref: "User" },
+        joinedAt: { type: Date, default: Date.now },
+        leftAt: { type: Date, default: null },
+      },
+    ],
+
+    startTime: {
+      type: Date,
+      default: Date.now,
+    },
+
+    endTime: {
+      type: Date,
+      default: null,
+    },
+
+    duration: {
+      type: Number / 60, // duration in minutes
+      default: null,
+    },
+  },
+  { timestamps: true },
+);
+
+// compound Index -> 1 : all sessions of a room, -1 : most recent first
+SessionSchema.index({ room: 1, createdAt: -1 });
+
+// Instance method to end the session and compute duration
+SessionSchema.methods.endSession = function () {
+  this.endTime = new Date();
+  this.duration = Math.floor((this.endTime - this.startTime) / 1000);
+  return this.save();
+};
+
+export default mongoose.model("Session", SessionSchema);
