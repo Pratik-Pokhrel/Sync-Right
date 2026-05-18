fc4c42014a1b5d775ba27b7724b4f5342d3577ec
Pratik-Pokhrel <prateek.pokhrel2@gmail.com>
2026-05-18 17:57:24 +0545

    feat: token generation added

--- server/config/env.js
+++ server/config/env.js
@@ -8,6 +8,10 @@ dotenv.config();
 export const ENV = {
   MONGO_URI: process.env.MONGO_URI,
   PORT: process.env.PORT || 5000,
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+  JWT_ACCESS_EXPIRY: process.env.JWT_ACCESS_EXPIRY || "15m",
+  JWT_REFRESH_EXPIRY: process.env.JWT_REFRESH_EXPIRY || "7d",
 };
 
 // Fail fast if any required environment variables are missing
---
+++ server/utils/generateTokens.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import { ENV } from "../config/env.js";
+
+// Payload : only non-sensitive info, like user ID and role
+export const generateAccessToken = (user) => {
+  return jwt.sign(
+    // Sign the payload with the secret key
+    { id: user._id, role: user.role },
+    ENV.JWT_ACCESS_SECRET,
+    { expiresIn: ENV.JWT_ACCESS_EXPIRY },
+  );
+};
+
+export const generateRefreshToken = (user) => {
+  return jwt.sign(
+    // Sign the payload with the secret key
+    { id: user._id },
+    ENV.JWT_REFRESH_SECRET,
+    { expiresIn: ENV.JWT_REFRESH_EXPIRY },
+  );
+};
+
+// what this file does is generate access and refresh tokens for a user. The access token contains the user's ID and role, and is signed with a secret key. The refresh token only contains the user's ID, and is also signed with a different secret key. Both tokens have an expiration time defined in the environment variables.
