1087f2c4534017c98400a447e49927fc6c1a416f
Pratik-Pokhrel <prateek.pokhrel2@gmail.com>
2026-05-27 08:02:56 +0545

    feat: oauth routes defined

---
+++ server/routes/oauth.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+  googleRedirect,
+  googleCallback,
+} from "../controllers/oauthController.js";
+
+const router = express.Router();
+
+router.get("/google", googleRedirect);
+router.get("/google/callback", googleCallback);
+
+export default router;
