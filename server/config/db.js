cde40cc8349ff435267e36446648fb60a2478af4
Pratik Pokhrel <prateek.pokhrel2@gmail.com>
2026-09-04 23:12:20 +0545

    refractor: modifications needed for deployment pt-4

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import { ENV } from "./env.js";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(ENV.MONGO_URI, { tls: false }); //tls: false is default but tls is required for some cloud providers like MongoDB Atlas
+    const conn = await mongoose.connect(ENV.MONGO_URI); //tls: false is default but tls is required for some cloud providers like MongoDB Atlas
     console.log(`MongoDB Connected : ${conn.connection.host}`);
   } catch (err) {
     console.log(`Database Connection failed: ${err.message}`);
